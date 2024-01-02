3fe29dc086624d23964cd230d119cea4c2727cb8
Shikhar Barthwal <barthwalshikhar@gmail.com>
2024-01-03 02:08:07 +0530

    uc1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Raghav");
+        User user = new User("Shikhar");
         System.out.println(user);
-        User user1 = new User("raghav");
+        User user1 = new User("Barthwal");
         System.out.println(user1);
     }
 }
